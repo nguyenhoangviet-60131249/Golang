6dd855cc981250cf7c7db7273cd4ff12f9b0e070
nguyenhoangviet <hoangviet3236@gmail.com>
2024-01-03 23:39:25 +0700

    update code arrays

--- arrays/arrays_declaration/main.go
+++ arrays/arrays_declaration/main.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello Golang")
+	// define array has 5 integers
+	var x [5]int
+	fmt.Print(x)
+
+	// define array have 5 strings
+	var y [8]string
+	fmt.Println(y)
+
+	// define array of 3 complex number
+	var z [3]complex128
+	fmt.Println(z)
 }
